51173306389b0b3d44a320365dbe336744fd9dcf
poove <poovendranselavaraj333@gmail.com>
2024-08-01 23:27:19 +0530

    1.40.22

--- carepulse/components/CustomFormField.tsx
+++ carepulse/components/CustomFormField.tsx
@@ -15,6 +15,11 @@ import { FormFieldType } from './forms/PatientForm'
 import Image from 'next/image'
 import 'react-phone-number-input/style.css'
 import PhoneInput from 'react-phone-number-input'
+import { E164Number } from 'libphonenumber-js/core'
+
+import DataPicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+
 
 interface CustomProps {
     control: Control<any>,
@@ -70,6 +75,22 @@ const RenderField = ({field, props}: {field: any; props: CustomProps}) => {
               />
           </FormControl>
         )  
+      case FormFieldType.DATE_PICKER:  
+        return (
+          <div className='flex rounded-md border border-dark-500 bg-dark-400'>
+            <Image 
+              src='/assets/icons/calendar.svg'
+              height={24} width={24}
+              alt='calendar'
+              className='ml-2'
+            />
+            <FormControl>
+              <DataPicker 
+              selected={field.value} 
+              onChange={(date) => field.onChange(date)}/>
+            </FormControl>
+          </div>
+        )
       default:
         break;
     }  
