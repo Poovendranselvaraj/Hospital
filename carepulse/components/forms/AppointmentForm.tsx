2a9d760beb6f39c6c426de724f958b8b27e145dc
poove <poovendranselavaraj333@gmail.com>
2024-08-06 20:28:52 +0530

    4.27 bug for date

--- carepulse/components/forms/AppointmentForm.tsx
+++ carepulse/components/forms/AppointmentForm.tsx
@@ -15,15 +15,17 @@ import { FormFieldType } from "./PatientForm"
 import { Doctors } from "@/constants"
 import { SelectItem } from "../ui/select"
 import Image from "next/image"
-import { createAppointment } from "@/lib/actions/appointment.actions"
+import { createAppointment, updateAppointment } from "@/lib/actions/appointment.actions"
+import { Appointment } from "@/types/appwrite.types"
 
 const AppointmentForm = ({
-    userId, patientId, type
+    userId, patientId, type, appointment, setOpen
 }: {
     userId: string;
     patientId: string;
     type: "create" | "cancel" | "schedule";
-
+    appointment?: Appointment;
+    setOpen: (open: boolean) => void;
 }) => {
     console.log("patientid",patientId)
     const router = useRouter();
@@ -34,11 +36,11 @@ const AppointmentForm = ({
     const form = useForm<z.infer<typeof AppointmentFormValidation>>({
         resolver: zodResolver(AppointmentFormValidation),
         defaultValues: {
-            primaryPhysician: "",
-            schedule: new Date(),
-            reason: "",
-            note: "",
-            cancellationReason: "",
+            primaryPhysician: appointment ? appointment.primaryPhysician:"",
+            schedule: appointment ? new Date(appointment.schedule) : new Date(),
+            reason: appointment? appointment.reason : "",
+            note: appointment? appointment.note : "",
+            cancellationReason: appointment? appointment.cancellationReason : "",
         },
     })
     async function onSubmit(values: z.infer<typeof AppointmentFormValidation>) {
@@ -73,7 +75,26 @@ const AppointmentForm = ({
                 if(appointment) {
                   form.reset();
                   router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);           }
+                } else{
+                    const appointmentToUpdate = {
+                        userId,
+                        appointmentId: appointment?.$id!,
+                        appointment: {
+                          primaryPhysician: values?.primaryPhysician,
+                          schedule: new Date(values?.schedule),
+                          status: status as Status,
+                          cancellationReason: values?.cancellationReason,
+                        },
+                        type
+                    }
+
+                    const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+                    if(updatedAppointment) {
+                        setOpen && setOpen(false);
+                        form.reset();
                 }
+            }
         } catch (error) {
             console.log(error); 
         }
@@ -100,11 +121,10 @@ const AppointmentForm = ({
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-                <section className="mb-12 space-y-4">
+                {type==="create" && <section className="mb-12 space-y-4">
                     <h1 className="header">New Appointment</h1>
                     <p className="text-dark-700">Request a new appointment in 10 seconds</p>
-                </section>
-
+                </section>}
                 {type !== "cancel" && (
                     <>
                         <CustomFormField
