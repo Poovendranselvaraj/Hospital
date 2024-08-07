7e7fec7c2c44c9ede87d375140e3a2baca9033b0
poove <poovendranselavaraj333@gmail.com>
2024-08-07 14:41:01 +0530

    5.16.49

--- carepulse/lib/actions/appointment.actions.ts
+++ carepulse/lib/actions/appointment.actions.ts
@@ -93,10 +93,10 @@ UpdateAppointmentParams) => {
         const smsMessage=`
           Hi, it's CarePulse.
           ${type==='schedule'?
-            `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!)}`
+            `Your appointment has been scheduled for ${formatDateTime(appointment.schedule!).dateTime}`
            : `We regret to inform you that your appointment has been cancelled for the following reason: ${appointment.cancellationReason}`
           }
-        ,
+        `
 
          await sendSMSNotification(userId, smsMessage);
         
